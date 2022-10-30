3807c9462d0e14e3fe01bd6d6261342a31f95d00
Woong <67966790+jiyko7@users.noreply.github.com>
2022-10-30 17:05:02 +0900

    [level 2] Title: 피보나치 수, Time: 3.15 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int[] array = new int[n+1];
+        
+        array[0] = 0;
+        array[1] = 1;
+        
+        for (int i = 2; i<= n; i++){
+            array[i] = (array[i-2]%1234567 + array[i-1]%1234567)%1234567;
+        }
+        
+        return array[n];
+    }
+    
+    
+}
