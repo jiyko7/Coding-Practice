9e46fe15996c304f688c29e02285a632fc5d9a4e
Woong <67966790+jiyko7@users.noreply.github.com>
2022-11-01 19:49:33 +0900

    [level 2] Title: 전화번호 목록, Time: 332.11 ms, Memory: 97.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i<phone_book.length-1; i++){
+            if (phone_book[i+1].length()>phone_book[i].length()){
+                if (phone_book[i+1].substring(0,phone_book[i].length()).equals(phone_book[i])){
+                    answer = false;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
