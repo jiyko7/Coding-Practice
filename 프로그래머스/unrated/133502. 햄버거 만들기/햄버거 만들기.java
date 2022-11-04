e3bc86e4d19e6daaa007383a2886974500240e84
Woong <67966790+jiyko7@users.noreply.github.com>
2022-11-04 23:02:44 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,53 +1,18 @@
-import java.util.Stack;
-
 class Solution {
     public int solution(int[] ingredient) {
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
         int answer = 0;
-		Stack<Integer> s = new Stack<>();
-		boolean check = true;
-		int start = 0;
-        
-		for (int ingre : ingredient) {
-			s.push(ingre);
-		}
-		
-		while (true) {
-			check = true;
-			for (int i = start; i<s.size()-3;i++) {
-				if (s.get(i)==1) {
-					if (checkburger(s,i+1,2)) {
-						if (checkburger(s,i+2,3)) {
-							if (checkburger(s,i+3,1)) {
-								check = false;
-								s.remove(i+3);
-								s.remove(i+2);
-								s.remove(i+1);
-								s.remove(i);
-                                if (i-3 >= 0){
-                                    start = i-3;
-                                }
-                                else{
-                                    start = 0;
-                                }
-								answer++;
-                                break;
-							}
-						}
-					}
-				}
-			}
-			
-			if (check) {
-				break;
-			}
-		}
-		return answer;
-	}
-	
-	static boolean checkburger(Stack<Integer> s, int index, int checknum ) {
-		if (s.get(index) == checknum) {
-			return true;
-		}
-		return false;
-	}
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
+                answer++;
+            }
+        }
+        return answer;
+    }
 }
