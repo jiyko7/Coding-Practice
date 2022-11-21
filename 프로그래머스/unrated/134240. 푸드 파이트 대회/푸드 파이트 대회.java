3e7fb4b4cc9b70d312e6b4835ea8fb8c7ce0cdc0
Woong <67966790+jiyko7@users.noreply.github.com>
2022-11-21 21:09:33 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 18.01 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for (int i =1; i<food.length; i++){
+            if (food[i]/2 > 0){
+                for (int j = 0; j<food[i]/2; j++){
+                    answer += i;
+                }
+            }    
+        }
+        
+        StringBuffer sb = new StringBuffer(answer);
+        String reverse = sb.reverse().toString();
+            
+            
+        return answer +"0" + reverse;
+    }
+}
